2febb41818ad88af46f2b30da66b99efa45f4099
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2023-01-29 11:15:50 +1100

    add eslint and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
-import { ColorModeScript } from "@chakra-ui/react"
-import * as React from "react"
-import * as ReactDOM from "react-dom/client"
-import { App } from "./App"
-import reportWebVitals from "./reportWebVitals"
-import * as serviceWorker from "./serviceWorker"
+import { ColorModeScript } from '@chakra-ui/react'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { App } from './App'
+import reportWebVitals from './reportWebVitals'
+import * as serviceWorker from './serviceWorker'
 
-
-const container = document.getElementById("root")
-if (!container) throw new Error('Failed to find the root element');
+const container = document.getElementById('root')
+if (!container) throw new Error('Failed to find the root element')
 const root = ReactDOM.createRoot(container)
 
 root.render(
-  <React.StrictMode>
-    <ColorModeScript />
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<ColorModeScript />
+		<App />
+	</React.StrictMode>
 )
 
 // If you want your app to work offline and load faster, you can change
@@ -26,4 +25,3 @@ serviceWorker.unregister()
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
-
